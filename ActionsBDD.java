ad9333dc3e0a4443b54fb30d66b6426afc907d7d
Rayan ZERIRI <118019083+Rayan-ZERIRI@users.noreply.github.com>
2024-12-31 14:17:43 +0100

    Update ActionsBDD.java

--- ActionsBDD.java
+++ ActionsBDD.java
@@ -1,5 +1,13 @@
-    public interface ActionsBDD {
-    void afficherProgrammeurs();
-    boolean supprimerProgrammeur(int id);
-    void terminerProgramme();
+public interface ActionsBDD {
+    public List<Programmeur> getTousProgrammeurs();
+
+    public Programmeur getProgrammeurInput(int id);
+
+    public boolean supprimerProgrammeurInput(int id);
+
+    public boolean ajouterProgrammeurInput(String nom, String prenom, int anNaissance, double salaire, double prime);
+
+    public boolean modifierSalaireInput(int id, double salaire);
+
+    public void terminerProgramme();
 }
